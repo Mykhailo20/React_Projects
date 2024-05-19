720a31ed00aa95cbdd17b0de03913383bfbbe894
Mykhailo20 <myhailo20189@gmail.com>
2024-05-19 14:10:55 +0300

    feat (dojo-blog): implement the display of the "Loading..." message while loading data from the db

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,27 +4,30 @@ import BlogList from './BlogList';
 const Home = () => {
     // create a reactive 'blogs' variable 
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     // runs every render / fires on every render
     useEffect(() => {
         // is often used to fetch data or communicate with authentication service
         // do not change state inside this hook if state is inside the dependencies (the second argument),
         // because you will end up with INFINITE LOOP
-        console.log('use effect ran');
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {
                 return res.json();
             })
             .then((data) => {
                 console.log(data);
                 setBlogs(data);
+                setIsPending(false);
             })
-
+        }, 1000); // setTimeout to simulate reading data from real db
     }, []);
 
     return (
         <div className="home">
-            { blogs && <BlogList blogs={ blogs } />}
+            { isPending && <div>Loading...</div> }
+            { blogs && <BlogList blogs={ blogs } title="All Blogs!"/>}
         </div>
     );
 }

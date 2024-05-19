6964b16416661d33267d1efd333e5d1bdcbe71a1
Mykhailo20 <myhailo20189@gmail.com>
2024-05-19 14:27:43 +0300

    feat (dojo-blog): implement handling fetch errors

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
     // create a reactive 'blogs' variable 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     // runs every render / fires on every render
     useEffect(() => {
@@ -13,19 +14,27 @@ const Home = () => {
         // because you will end up with INFINITE LOOP
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then((data) => {
-                console.log(data);
-                setBlogs(data);
-                setIsPending(false);
-            })
+                .then(res => {
+                    if(!res.ok) {
+                        throw Error('Could not fetch the data for the specified resource.');
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    setBlogs(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setError(err.message);
+                    setIsPending(false);
+                })
         }, 1000); // setTimeout to simulate reading data from real db
     }, []);
 
     return (
         <div className="home">
+            { error && <div>{ error }</div> }
             { isPending && <div>Loading...</div> }
             { blogs && <BlogList blogs={ blogs } title="All Blogs!"/>}
         </div>

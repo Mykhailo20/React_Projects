a20d2eb5790cc9c91de7e8a1c9af78de1d221931
Mykhailo20 <myhailo20189@gmail.com>
2024-05-19 14:40:59 +0300

    feat (dojo-blog): make a custom 'useFetch' hook

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,36 +1,9 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    // create a reactive 'blogs' variable 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    // runs every render / fires on every render
-    useEffect(() => {
-        // is often used to fetch data or communicate with authentication service
-        // do not change state inside this hook if state is inside the dependencies (the second argument),
-        // because you will end up with INFINITE LOOP
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if(!res.ok) {
-                        throw Error('Could not fetch the data for the specified resource.');
-                    }
-                    return res.json();
-                })
-                .then((data) => {
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch(err => {
-                    setError(err.message);
-                    setIsPending(false);
-                })
-        }, 1000); // setTimeout to simulate reading data from real db
-    }, []);
+    const { data:blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
     return (
         <div className="home">
---
+++ dojo-blog/src/useFetch.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+    // create a reactive 'blogs' variable 
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    // runs every render / fires on every render
+    useEffect(() => {
+        // is often used to fetch data or communicate with authentication service
+        // do not change state inside this hook if state is inside the dependencies (the second argument),
+        // because you will end up with INFINITE LOOP
+        setTimeout(() => {
+            fetch(url)
+                .then(res => {
+                    if(!res.ok) {
+                        throw Error('Could not fetch the data for the specified resource.');
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    setData(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setError(err.message);
+                    setIsPending(false);
+                })
+        }, 1000); // setTimeout to simulate reading data from real db
+    }, [url]);
+
+    return {data, isPending, error };
+}
+
+export default useFetch;

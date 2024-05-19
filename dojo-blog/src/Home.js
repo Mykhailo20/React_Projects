4a860efaac78746d4f854e14ecd13adbb5575bd5
Mykhailo20 <myhailo20189@gmail.com>
2024-05-19 14:01:11 +0300

    feat (dojo-blog): fetch data from JSON server

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,23 +1,30 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
     // create a reactive 'blogs' variable 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    // runs every render / fires on every render
+    useEffect(() => {
+        // is often used to fetch data or communicate with authentication service
+        // do not change state inside this hook if state is inside the dependencies (the second argument),
+        // because you will end up with INFINITE LOOP
+        console.log('use effect ran');
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setBlogs(data);
+            })
+
+    }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={ blogs } handleDelete={ handleDelete } />
+            { blogs && <BlogList blogs={ blogs } />}
         </div>
     );
 }

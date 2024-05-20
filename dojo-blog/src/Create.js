7ec65c8efac4dcfbaf69d97b019372412e79e952
Mykhailo20 <myhailo20189@gmail.com>
2024-05-20 16:20:52 +0300

    feat (dojo-blog): redirect the user to the 'Home' page after adding a new blog

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react"; 
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -17,8 +19,10 @@ const Create = () => {
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(blog)
         }).then(() => {
-            console.log('New blog added');
             setIsPending(false);
+            // history.go(-n) - go back n pages in history
+            // history.go(n) - go forward n pages in history
+            history.push('/'); // redirect user to a 'Home' page
         })
     }
 
